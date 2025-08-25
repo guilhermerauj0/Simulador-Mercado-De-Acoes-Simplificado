128926f076dd651a531ba0dfa7f073b96e59c9f1
guilhermerauj0 <arthurguilhermesb@gmail.com>
2025-08-25 16:45:13 -0300

    Add class Acao with her methods

---
+++ src/Acao.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Acao {
+    String codigo;
+    double preco;
+
+    List<Investidor> investidores;
+    public Acao(String codigo, double preco) {
+        this.codigo = codigo;
+        investidores = new ArrayList<>();
+    }
+
+    public void adicionarInestidor(Investidor investidor) {
+        investidores.add(investidor);
+    }
+
+    public void removerInvestidor(Investidor investidor) {
+        investidores.remove(investidor);
+    }
+
+    public void notificarInvestidores() {
+        for (Investidor investidor : investidores) {
+            investidor.atualizar(codigo, preco);
+        }
+    }
+
+    public void setPreco(double novoPreco) {
+        this.preco = novoPreco;
+        notificarInvestidores();
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+
+}
